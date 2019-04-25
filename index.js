1adb5b9b987dbef19d8c0e696f1f08217b5b37e7
navneetagrwl <navneetagrawal1301@gmail.com>
2019-04-25 22:17:07 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/app/App';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
 
 render(<App />, document.getElementById('root'));
