dcfe6c3c27a0c6e3638c9cfad7f034c124627c8a
navneetagrwl <navneetagrawal1301@gmail.com>
2019-04-25 22:23:27 +0530

    1 file modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import Hello from './Hello';
 import './style.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './component/app/App';
+import App from './Container/App/app';
 
 
 render(<App />, document.getElementById('root'));
